c6e50630a005118c1ff02ba48860588b0737b45c
riversword <2694473261@qq.com>
2017-10-29 16:48:58 +0800

    代码格式规范化
    js还有待优化

--- Tic Tac Toe Game.js
+++ Tic Tac Toe Game.js
@@ -3,34 +3,26 @@
   Copyright ©2017 riversword. All rights reserved.
 */
 $(document).ready(function(){
-	//模式选择，perVSper,perVScom(对战信息，棋子样式)
-	/*for(var i=0;i<9;i++){
-		console.log($('.vsInfo table td').eq(i).html());	
-	}*/
+	
 	var InfoTrs=$('.vsInfo tr'),a=[],b=[],p1Ws=0,cWs=0,p2Ws=0;
 	var lastWinner='player1',f,fOwner,s,sOwner,curTurn,curPointer,gameMode='player1 VS computer',isWin,cMap=[];
 	var per=[],com=[],roomLeft=[0,1,2,3,4,5,6,7,8],cent=[4],jiao=[0,2,6,8],winWay=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],victoryBox=[];
+	
 	for(var i=0;i<3;i++){
 		a[i]=$(InfoTrs[i].getElementsByTagName('td')[1]);
 		b[i]=$(InfoTrs[i].getElementsByTagName('td')[0]);
-		//console.log(a[i]);  注意jquery对象与dom对象的不同规则	
 	}
+
 	readyStart();
-	//落棋
+
 	$('.gameMap table td').click(function(){
 		if(gameMode=='player1 VS computer'){
 			if($(this).html()==''){
 				$(this).html(a[0].html());
 				per.push(parseInt(this.id));
 				delFAll(parseInt(this.id));
-				//console.log(per);
-				//console.log(cent);
-				//console.log(jiao);
-				//console.log(roomLeft);
-				console.log('per是'+per);
-				console.log('com是'+com);
 				hasWin();
-				//console.log(isWin);
+
 				if(!isWin){//人未赢
 					if(isFulled()){
 						$('.tipInfo p').html("It's a tie.");
@@ -50,22 +42,13 @@ $(document).ready(function(){
 						}
 					}
 				}
-				//nextTurn();//curTurn切换
-				//执行电脑下棋函数（包含curTurn切换）
-				//console.log('per是'+per);
-				//console.log('cent是'+cent);
-				//console.log('jiao是'+jiao);
-				//console.log('roomLeft是'+roomLeft);
 				
 			}
 		}else{
 			//player1 VS player2模式
 			if($(this).html()==''){
 				$(this).html(curPointer);
-				//console.log($(this).attr('id'));
-				//console.log(this.id);
 				hasWin();
-				//console.log(isWin);
 				if(!isWin){
 					if(isFulled()){
 						$('.tipInfo p').html("It's a tie.");
@@ -121,18 +104,21 @@ $(document).ready(function(){
 		clearRecord();
 		readyStart();
 	});
+
+	//鼠标悬停在computer/player2时，显示提示可切换模式
 	b[1].mouseover(function(){
 		$('.vsInfo div.changeMode').removeClass("disappear");
-		$(this).mouseout(function(){//这里的this指向b[1]
-		$('.vsInfo div.changeMode').addClass("disappear");
-		$('.vsInfo div.changeMode').mouseover(function(){
-			$(this).removeClass("disappear");//这里的this指向$('.vsInfo div.changeMode')
-			$(this).mouseout(function(){
-				$(this).addClass("disappear");
+		$(this).mouseout(function(){ //这里的this指向b[1]
+			$('.vsInfo div.changeMode').addClass("disappear");
+			$('.vsInfo div.changeMode').mouseover(function(){
+				$(this).removeClass("disappear"); //这里的this指向$('.vsInfo div.changeMode')
+				$(this).mouseout(function(){
+					$(this).addClass("disappear");
+				});
 			});
 		});
 	});
-	});
+
 	b[2].mouseover(function(){
 		$('.vsInfo div.changeMode').removeClass("disappear");
 		$(this).mouseout(function(){
@@ -145,7 +131,7 @@ $(document).ready(function(){
 		});
 	});
 	});
-	
+	//点击切换模式
 	b[1].click(function(){
 		$('.vsInfo tr:eq(2)').removeClass("disappear");//元素被设置为display:none;后，元素仍存在，获取元素时仍须将其算在内。
 		$('.vsInfo tr:eq(1)').addClass("disappear");
@@ -161,18 +147,20 @@ $(document).ready(function(){
 		readyStart();
 	});
 	
+	//点击play again 重新开始
 	$('.tipInfo button').click(function(){
 		//隐藏cover层
 		readyStart();
 	});
 
 	function readyStart(){
-		//
+		
 		$('.tipInfo button:eq(0)').removeClass('disappear');
 		$('.tipInfo button:eq(1)').addClass('disappear');
-		$('.tipInfo p').html('The winner is <span></span>');
+		$('.tipInfo p').html('The winner is <span></span>');//清空上一次的胜利者信息
 		$('.cover').addClass('disappear');
 
+		//清空游戏地图格子
 		for(var i=0;i<9;i++){
 			$('.gameMap table td').html('');
 			cMap[i]='';
@@ -182,7 +170,7 @@ $(document).ready(function(){
 			//true,即当前为player1 VS computer
 			gameMode='player1 VS computer';
 			if(lastWinner=='player1'){
-				//状态显示player1's turn
+				
 				fOwner='player1',f=a[0].html();
 				sOwner='computer',s=a[1].html();
 			}else{
@@ -194,7 +182,7 @@ $(document).ready(function(){
 			//false,即当前为player1 VS player2
 			gameMode='player1 VS player2';
 			if(lastWinner=='player1'){
-				//状态显示player1's turn
+				
 				fOwner='player1',f=a[0].html();
 				sOwner='player2',s=a[2].html();
 			}else{
@@ -203,20 +191,14 @@ $(document).ready(function(){
 			}
 			curTurn=fOwner,curPointer=f;
 		}
-		//与电脑下棋时，不需要curTurn
-		/*if(curTurn=='computer'){
-			//执行电脑下棋函数
-			curTurn=sOwner,curPointer=s;
-		}*/
+
 	}
+
+	//判断是否有胜利者产生
 	function hasWin(){
 		isWin=false;
 		if($('.gameMap table td').eq(0).html()==$('.gameMap table td').eq(1).html() && $('.gameMap table td').eq(1).html()==$('.gameMap table td').eq(2).html() && $('.gameMap table td').eq(1).html()!=''){
-			//判断胜利者是？
 			//console.log('第一行');
-			//console.log($('.gameMap table td').eq(0).html());
-			//console.log($('.gameMap table td').eq(1).html());
-			//console.log($('.gameMap table td').eq(2).html());
 			if($('.gameMap table td').eq(0).html()==f){
 				lastWinner=fOwner;
 			}else{
@@ -226,7 +208,6 @@ $(document).ready(function(){
 			showWinner();
 		}
 		if($('.gameMap table td').eq(3).html()==$('.gameMap table td').eq(4).html() && $('.gameMap table td').eq(4).html()==$('.gameMap table td').eq(5).html() && $('.gameMap table td').eq(4).html()!=''){
-			//判断胜利者是？
 			//console.log('第二行');
 			if($('.gameMap table td').eq(3).html()==f){
 				lastWinner=fOwner;
@@ -237,7 +218,6 @@ $(document).ready(function(){
 			showWinner();
 		}
 		if($('.gameMap table td').eq(6).html()==$('.gameMap table td').eq(7).html() && $('.gameMap table td').eq(7).html()==$('.gameMap table td').eq(8).html() && $('.gameMap table td').eq(7).html()!=''){
-			//判断胜利者是？
 			//console.log('第三行');
 			if($('.gameMap table td').eq(6).html()==f){
 				lastWinner=fOwner;
@@ -248,7 +228,6 @@ $(document).ready(function(){
 			showWinner();
 		}
 		if($('.gameMap table td').eq(0).html()==$('.gameMap table td').eq(3).html() && $('.gameMap table td').eq(3).html()==$('.gameMap table td').eq(6).html() && $('.gameMap table td').eq(3).html()!=''){
-			//判断胜利者是？
 			//console.log('第一列');
 			if($('.gameMap table td').eq(0).html()==f){
 				lastWinner=fOwner;
@@ -259,7 +238,6 @@ $(document).ready(function(){
 			showWinner();
 		}
 		if($('.gameMap table td').eq(1).html()==$('.gameMap table td').eq(4).html() && $('.gameMap table td').eq(4).html()==$('.gameMap table td').eq(7).html() && $('.gameMap table td').eq(4).html()!=''){
-			//判断胜利者是？
 			//console.log('第二列');
 			if($('.gameMap table td').eq(1).html()==f){
 				lastWinner=fOwner;
@@ -270,7 +248,6 @@ $(document).ready(function(){
 			showWinner();
 		}
 		if($('.gameMap table td').eq(2).html()==$('.gameMap table td').eq(5).html() && $('.gameMap table td').eq(5).html()==$('.gameMap table td').eq(8).html() && $('.gameMap table td').eq(5).html()!=''){
-			//判断胜利者是？
 			//console.log('第三列');
 			if($('.gameMap table td').eq(2).html()==f){
 				lastWinner=fOwner;
@@ -281,7 +258,6 @@ $(document).ready(function(){
 			showWinner();
 		}
 		if($('.gameMap table td').eq(0).html()==$('.gameMap table td').eq(4).html() && $('.gameMap table td').eq(4).html()==$('.gameMap table td').eq(8).html() && $('.gameMap table td').eq(4).html()!=''){
-			//判断胜利者是？
 			//console.log('左上角斜');
 			if($('.gameMap table td').eq(0).html()==f){
 				lastWinner=fOwner;
@@ -292,7 +268,6 @@ $(document).ready(function(){
 			showWinner();
 		}
 		if($('.gameMap table td').eq(2).html()==$('.gameMap table td').eq(4).html() && $('.gameMap table td').eq(4).html()==$('.gameMap table td').eq(6).html() && $('.gameMap table td').eq(4).html()!=''){
-			//判断胜利者是？
 			//console.log('右上角斜');
 			if($('.gameMap table td').eq(2).html()==f){
 				lastWinner=fOwner;
@@ -303,35 +278,36 @@ $(document).ready(function(){
 			showWinner();
 		}
 	}
+
 	function showWinner(){
 		switch(lastWinner){
 				case 'player1':p1Ws++;
 					$('.vsInfo table td').eq(2).html(p1Ws+' win');
 					$('.tipInfo p span').html(lastWinner);
-					$('.cover').removeClass('disappear');//弹出restart的窗口；
+					$('.cover').removeClass('disappear');//弹出play again的窗口；
 					break;
 				case 'computer':cWs++;
 					$('.vsInfo table td').eq(5).html(cWs+' win');
 					$('.tipInfo p span').html(lastWinner);
-					$('.cover').removeClass('disappear');//弹出restart的窗口；
+					$('.cover').removeClass('disappear');
 					break;
 				case 'player2':p2Ws++;
 					$('.vsInfo table td').eq(8).html(p2Ws+' win');
 					$('.tipInfo p span').html(lastWinner);
-					$('.cover').removeClass('disappear');//弹出restart的窗口；
+					$('.cover').removeClass('disappear');
 					break;
 			}
 	}
+
+	//清空游戏记录
 	function clearRecord(){
 		lastWinner='player1';
 		p1Ws=0,cws=0,p2Ws=0;
 		$('.vsInfo table td').eq(2).html(p1Ws+' win');
 		$('.vsInfo table td').eq(5).html(cWs+' win');
 		$('.vsInfo table td').eq(8).html(p2Ws+' win');
-		/*for(var i=0;i<9;i++){
-			$('.gameMap table td').html('');
-		}*/
 	}
+
 	function nextTurn(){
 		//首先判断游戏模式
 		if(gameMode=='player1 VS computer'){
@@ -349,6 +325,7 @@ $(document).ready(function(){
 			}
 		}
 	}
+
 	function isFulled(){
 		var result=true;
 		for(var i=0;i<9;i++){
@@ -360,31 +337,32 @@ $(document).ready(function(){
 		return result;
 	}
 
+	//电脑的下棋策略
 	function comBrain(){
-		var step4j;//=jiao[Math.floor(jiao.length*Math.random())];
+		var step4j;
 		//这是电脑的第三步棋及以后几步，中间位置必然已被占
 		if(com.length>1){
 			//电脑先下或是人先下都是如此处理
 				if(willWin(com,per)){
-					console.log('我赢了！');
+					//console.log('我赢了！');
 					var num2=Math.floor(Math.random()*victoryBox.length);
 					$('.gameMap table td').eq(victoryBox[num2]).html(a[1].html());
 					com.push(victoryBox[num2]);
 					delFAll(victoryBox[num2]);
 				}else if(willWin(per,com)){
-					console.log('人要赢，电脑第三步及以后');
+					//console.log('人要赢，电脑第三步及以后');
 					var num2=Math.floor(Math.random()*victoryBox.length);
 					$('.gameMap table td').eq(victoryBox[num2]).html(a[1].html());
 					com.push(victoryBox[num2]);
 					delFAll(victoryBox[num2]);
 				}else if(jiao.length>0){//还有角，则下角
-					console.log('电脑第三步及以后，随机下角上');
+					//console.log('电脑第三步及以后，随机下角上');
 					var num2=Math.floor(Math.random()*jiao.length);
 					$('.gameMap table td').eq(jiao[num2]).html(a[1].html());
 					com.push(jiao[num2]);
 					delFAll(jiao[num2]);
 				}else{//随便空位，下一个子
-					console.log('电脑第三步及以后，随机下个地方');
+					//console.log('电脑第三步及以后，随机下个地方');
 					var num2=Math.floor(Math.random()*roomLeft.length);
 					$('.gameMap table td').eq(roomLeft[num2]).html(a[1].html());
 					com.push(roomLeft[num2]);
@@ -418,12 +396,12 @@ $(document).ready(function(){
 					com.push(victoryBox[num1]);
 					delFAll(victoryBox[num1]);
 				}else if(cent.length==1){//中间位置空着就下中间
-					console.log('电脑第二步，下中间');
+					//console.log('电脑第二步，下中间');
 					$('.gameMap table td').eq(4).html(a[1].html());
 					com.push(4);
 					delFAll(4);
 				}else{//下在角上
-					console.log('电脑第二步，随机下角上');
+					//console.log('电脑第二步，随机下角上');
 					var num1=Math.floor(Math.random()*jiao.length);
 					$('.gameMap table td').eq(jiao[num1]).html(a[1].html());
 					com.push(jiao[num1]);
@@ -456,8 +434,8 @@ $(document).ready(function(){
 			}
 		}
 		
-		
 	}
+
 	function delOne(arr,y){//删除数组arr中的y元素
 		var ind=arr.indexOf(y);
 		if(ind!=-1){
@@ -470,6 +448,7 @@ $(document).ready(function(){
 		delOne(jiao,y);
 	}
 	
+	//判断当前是否可以下一棋胜利
 	function willWin(arr,arg){
 		//数组arr中的元素,每两个形成一个组合。arr是player1下的位置,arg是computer下的位置
 		victoryBox=[];//每次调用该函数要将victoryBox初始化
@@ -482,27 +461,26 @@ $(document).ready(function(){
 				arr2.push(box);
 			}
 		}
-		//console.log("arr2是");
-		//console.log(arr2);
+		
 		//获得了组合arr2,再做判断是否具有胜利条件？可不可以在获得组合的过程中，判断是否具有胜利条件呢？(筛选组合)
 		var canVictory=false;
 		for(var i=1;i<arr2.length;i++){//arr的第一个元素是空数组
-			console.log('arr2['+i+']='+arr2[i]);
+			//console.log('arr2['+i+']='+arr2[i]);
 			if(inTheWay(arr2[i])!=-1){
 				var has=true;
 				for(var j=0;j<arg.length;j++){
 					if(winWay[inTheWay(arr2[i])].indexOf(arg[j]) !=-1){//元素arg[j]在该方法中(已经阻挡了对手)
-						console.log('arg['+j+']='+arg[j]);//这一句为何不起作用
+						//console.log('arg['+j+']='+arg[j]);//这一句为何不起作用
 						has=false;//在该方法中没有胜利位置
 						break;
 					}
 				}
-				console.log('has='+has);
+				//console.log('has='+has);
 				if(has){
 					var victory=winWay[inTheWay(arr2[i])];
 					delOne(victory,arr2[i][0]);
 					delOne(victory,arr2[i][1]);
-					console.log('victory='+victory);
+					//console.log('victory='+victory);
 					victoryBox.push(victory[0]);//victory[0]即胜利位置
 					canVictory=true;
 				}
